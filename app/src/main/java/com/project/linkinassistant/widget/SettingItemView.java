f368835a26dae0faad41c1896ed7b0f055a5aa2a
zhimin.liu <zhimin8090186@163.com>
2017-08-17 11:03:20 +0800

    feat: add declare-styleable
    Signed-off-by: zhimin.liu <zhimin8090186@163.com>

--- app/src/main/java/com/project/linkinassistant/widget/SettingItemView.java
+++ app/src/main/java/com/project/linkinassistant/widget/SettingItemView.java
@@ -1,9 +1,11 @@
 package com.project.linkinassistant.widget;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.project.linkinassistant.R;
 
@@ -18,7 +20,24 @@ public class SettingItemView extends RelativeLayout {
 
     public SettingItemView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SettingItemView);
+        String title = a.getString(R.styleable.SettingItemView_titles);
+        int bgType = a.getInt(R.styleable.SettingItemView_bgType, -1);
+        a.recycle();
+
         View view = View.inflate(context, R.layout.view_setting_item, null);
         addView(view);
+
+        TextView tVtitle= (TextView) view.findViewById(R.id.view_setting_title);
+        tVtitle.setText(title);
+        switch (bgType) {
+            case 0:
+                view.setBackgroundResource(R.drawable.first_normal);
+                break;
+            case 1:
+                view.setBackgroundResource(R.drawable.last_normal);
+                break;
+        }
     }
 }
